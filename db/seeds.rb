15d15df8a715bba9285f5ccc21fd95d59f2b8bac
Susan Davies <sdav9375@gmail.com>
2016-06-03 21:00:35 -0400

    file name too long error

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,20 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'open-uri'
 require 'csv'
 
 csv_text = File.read(Rails.root.join('lib', 'seeds', 'appt_data.csv'))
-csv = CSV.parse(csv_text, :headers => true)
+csv = CSV.open(csv_text, :headers => true, :encoding => 'ISO-8859-1', :row_sep => "\r\n", :col_sep => ";")
 csv.each do |row|
-  Appointment.create!(row.to_hash)
+  t = Appointment.new
+  t.first_name = row['first_name']
+  t.last_name = row['last_name']
+  t.comments = row['comments']
+  t.start_time = row['start_time']
+  t.end_time = row['end_time']
+  t.save
+  puts "#{t.first_name}, #{t.last_name} saved"
 end
+
+puts "There are now #{Appointment.count} rows in the appointments table"
